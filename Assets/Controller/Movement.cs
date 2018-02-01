11170f0e297372147e98eebef1d1f902c554732d
Benjamin Strasser <bp.strasser@gmail.com>
2018-02-01 20:28:16 +0100

    First commit

---
+++ Assets/Controller/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Vector3 moveToward;
+    public int speed = 5;
+    Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            var mousePos = Input.mousePosition;
+            moveToward = Camera.main.ScreenToWorldPoint(mousePos);
+            //Debug.Log("Preesed button "+ moveToward + "_____" + rb2d.position.x + " : " + rb2d.position.y);
+
+        }
+        Vector2 dif = new Vector2(moveToward.x - rb2d.position.x, moveToward.y - rb2d.position.y);
+        //Debug.Log(dif.magnitude);
+        if (dif.magnitude > 0.50) {
+
+            dif.Normalize();
+            dif.x *= speed;
+            dif.y *= speed;
+
+            rb2d.velocity = dif;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2();
+        }
+
+	}
+}
---
+++ Assets/Controller/ProjController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjController : MonoBehaviour
+{
+    public int amount = 500;
+    public GameObject prefab;
+    float time = 0.5f;
+    float timer;
+
+    // Use this for initialization
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetMouseButton(1) && timer > time)
+        {
+            timer = 0;
+            //Debug.Log("mouse pressed");
+            Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = (Input.mousePosition - sp).normalized;
+
+            GameObject proj = Instantiate(prefab,gameObject.transform);
+            proj.GetComponent<Rigidbody2D>().velocity = dir * amount;
+            //proj.GetComponent<Rigidbody2D>().AddForce(dir * amount);
+        }
+    }
+}
